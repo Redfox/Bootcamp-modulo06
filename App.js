0174706867fc1e3e4714c66fffa4a3e0dfbc5d06
VitorRedfox <vitor_pereira.2013@hotmail.com>
2019-08-31 18:39:08 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Wellcome</Text>
+    </View>
+  );
+};
+
+export default App;
