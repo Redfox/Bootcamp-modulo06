36071b34b36d809314371aee7f568226e90cb6f5
VitorRedfox <vitor_pereira.2013@hotmail.com>
2019-09-01 11:23:17 -0300

    feat: list starred

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,15 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
 import api from '../../services/api';
 
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
+
 export default function User({ navigation }) {
+  const user = navigation.getParam('user');
   const [stars, setStars] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
-      const user = navigation.getParam('user');
-      const response = await api.get(`/users/${user.login}/starred`);
+      const storageUser = navigation.getParam('user');
+      const response = await api.get(`/users/${storageUser.login}/starred`);
 
       setStars(response.data);
     }
@@ -17,7 +31,29 @@ export default function User({ navigation }) {
     fetchData();
   }, []);
 
-  return <View />;
+  return (
+    <Container>
+      <Header>
+        <Avatar source={{ uri: user.avatar }} />
+        <Name>{user.name}</Name>
+        <Bio>{user.bio}</Bio>
+      </Header>
+
+      <Stars
+        data={stars}
+        keyExtractor={star => String(star.id)}
+        renderItem={({ item }) => (
+          <Starred>
+            <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+            <Info>
+              <Title>{item.name}</Title>
+              <Author>{item.owner.login}</Author>
+            </Info>
+          </Starred>
+        )}
+      />
+    </Container>
+  );
 }
 
 User.navigationOptions = ({ navigation }) => ({
