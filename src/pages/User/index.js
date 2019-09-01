9ca70c9635a724fc9ccc3b5b4627ea2ce75925c9
VitorRedfox <vitor_pereira.2013@hotmail.com>
2019-09-01 11:03:31 -0300

    feat: get starred github user

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { View } from 'react-native';
+import api from '../../services/api';
 
 export default function User({ navigation }) {
-  console.tron.log(navigation.getParam('user'));
+  const [stars, setStars] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const user = navigation.getParam('user');
+      const response = await api.get(`/users/${user.login}/starred`);
+
+      setStars(response.data);
+    }
+
+    fetchData();
+  }, []);
+
   return <View />;
 }
+
+User.navigationOptions = ({ navigation }) => ({
+  title: navigation.getParam('user').name,
+});
 
 User.propTypes = {
   navigation: PropTypes.shape({
